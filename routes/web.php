7f0c7b0cbc0f0e35cda264cfb7f18b1cca899abd
Graeme Xyber Pastoril <xyber.pastoril@gmail.com>
2022-04-21 20:19:06 +0800

    Temporarily use resources from `xyberpastoril/TakeawayRepository`

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Authentication Routes
+Route::get('login', [\App\Http\Controllers\Auth\LoginController::class, 'showLoginForm'])->name('login');
+Route::post('login', [\App\Http\Controllers\Auth\LoginController::class, 'login']);
+Route::post('logout', [\App\Http\Controllers\Auth\LoginController::class, 'logout'])->name('logout');
 
-Auth::routes();
+// Registration Routes...
+Route::get('register', [\App\Http\Controllers\Auth\RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('register', [\App\Http\Controllers\Auth\RegisterController::class, 'register']);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Password Reset Routes...
+// Route::get('password/reset', [\App\Http\Controllers\Auth\ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
+// Route::post('password/email', [\App\Http\Controllers\Auth\ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
+// Route::get('password/reset/{token}', [\App\Http\Controllers\Auth\ResetPasswordController::class, 'showResetForm'])->name('password.reset');
+// Route::post('password/reset', [\App\Http\Controllers\Auth\ResetPasswordController::class, 'reset'])->name('password.update');
+
+// Route::group([/*'middleware' => 'auth'*/], function() {
+    // Route::view('/', '')->name('home');
+    // Route::resource('/source', \App\Http\Controllers\SourceController::class)->except('create', 'edit');
+    // Route::view('/settings', 'settings.index')->name('settings');
+// });
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
