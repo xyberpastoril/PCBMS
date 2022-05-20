4ceb1a35f6bb6eeb3c0a2bb58b5c78c1b342a32b
Graeme Xyber Pastoril <xyber.pastoril@gmail.com>
2022-05-20 13:54:21 +0800

    Add custom css to remove up and down arrows for numeric input fields

--- resources/views/includes/global-styles.blade.php
+++ resources/views/includes/global-styles.blade.php
@@ -1,2 +1,3 @@
 <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
 <link href="{{ url('/css/styles.css') }}" rel="stylesheet" />
+<link rel="stylesheet" href="{{ url('/css/form.css') }}">
