f417a56a202258249ba4ddc677a91e9529bc5d84
Graeme Xyber Pastoril <xyber.pastoril@gmail.com>
2022-05-20 14:51:50 +0800

    Install `Tagify` JS Package

--- resources/views/includes/global-scripts.blade.php
+++ resources/views/includes/global-scripts.blade.php
@@ -1,1 +1,5 @@
+{{-- Packages CDN --}}
 <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+<script src="https://unpkg.com/@yaireo/tagify"></script>
+<script src="https://unpkg.com/@yaireo/tagify/dist/tagify.polyfills.min.js"></script>
+
--- resources/views/includes/global-styles.blade.php
+++ resources/views/includes/global-styles.blade.php
@@ -1,3 +1,7 @@
+{{-- Packages CDN --}}
 <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
+<link href="{{ url('/css/tagify.css') }}" rel="stylesheet" type="text/css" />
+
+{{-- Custom --}}
 <link href="{{ url('/css/styles.css') }}" rel="stylesheet" />
 <link rel="stylesheet" href="{{ url('/css/form.css') }}">
