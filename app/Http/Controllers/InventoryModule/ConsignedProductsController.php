ada9233482c3ae4c12b601f4ff7cb92cf7e95419
Graeme Xyber Pastoril <xyber.pastoril@gmail.com>
2022-06-04 15:21:27 +0800

    Fixed consigned product search

--- app/Http/Controllers/InventoryModule/ConsignedProductsController.php
+++ app/Http/Controllers/InventoryModule/ConsignedProductsController.php
@@ -14,12 +14,14 @@ class ConsignedProductsController extends Controller
         $units = ConsignedProduct::select(
             'consigned_products.uuid as value',
             'consigned_products.id',
-            DB::raw("CONCAT(products.name, ' (', consigned_products.particulars, units.abbreviation, ')') as product"),
+            DB::raw("CONCAT(products.name, ' (', consigned_products.particulars, units.abbreviation, ')') as name"),
+            'consigned_products.sale_price',
         )
         ->leftJoin('products', 'products.id', '=', 'consigned_products.product_id')
         ->leftJoin('units', 'units.id', '=', 'products.unit_id')
-        ->where('units.name', 'LIKE', "%{$query}%")
-        ->orWhere('units.abbreviation', 'LIKE', "%{$query}%")
+        ->where('products.name', 'LIKE', "%{$query}%")
+        ->orWhere('consigned_products.particulars', 'LIKE', "%{$query}%")
+        ->orWhere('consigned_products.id', 'LIKE', "%{$query}%")
         ->limit(5)
         ->get();
 
