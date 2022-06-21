f7af30df2860c7b8aee2a4fd0acc69b6cfe29414
Graeme Xyber Pastoril <xyber.pastoril@gmail.com>
2022-06-21 15:58:03 +0800

    Fix tagify CDN not working as expected

--- resources/views/includes/global-scripts.blade.php
+++ resources/views/includes/global-scripts.blade.php
@@ -1,6 +1,6 @@
 {{-- Packages CDN --}}
 <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
-<script src="https://unpkg.com/@yaireo/tagify"></script>
+<script src="https://unpkg.com/@yaireo/tagify@4.12.0/dist/tagify.min.js"></script>
 <script src="https://unpkg.com/@yaireo/tagify/dist/tagify.polyfills.min.js"></script>
 
 {{-- Custom --}}
